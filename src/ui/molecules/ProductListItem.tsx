24cbb740e6e3d86272aa904e9099aecf71f66b67
Daniel Gola <danielgola@daniels-air.home>
2024-02-06 22:46:31 +0100

    feat(MODULE-1): add components and render list of products

---
+++ src/ui/molecules/ProductListItem.tsx
@@ -0,0 +1,18 @@
+import { type Product } from "@/types/product";
+import { ProductCoverImage } from "@/ui/atoms/ProductCoverImage";
+import { ProductDescription } from "@/ui/atoms/ProductDescription";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const ProductListItem = ({
+  product,
+}: ProductListItemProps): JSX.Element => (
+  <li>
+    <article>
+      <ProductCoverImage {...product.coverImage} />
+      <ProductDescription product={product} />
+    </article>
+  </li>
+);
