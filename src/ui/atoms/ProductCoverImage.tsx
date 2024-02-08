160d24e4ae057c14dd1534b4ebfe97b36d881833
Daniel Gola <danielgola@daniels-air.home>
2024-02-08 12:35:27 +0100

    feat(MODULE-1): add apollo server with sqlite

--- src/ui/atoms/ProductCoverImage.tsx
+++ src/ui/atoms/ProductCoverImage.tsx
@@ -1,21 +1,18 @@
 import { type Product } from "@/types/product";
 
 type ProductCoverImageProps = {
-  src: Product["coverImage"]["src"];
-  alt: Product["coverImage"]["alt"];
+	src: Product["coverImage"]["src"];
+	alt: Product["coverImage"]["alt"];
 };
 
-export const ProductCoverImage = ({
-  src,
-  alt,
-}: ProductCoverImageProps): JSX.Element => (
-  <div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-50">
-    <img
-      width={320}
-      height={320}
-      alt={alt}
-      src={src}
-      className="h-full w-full object-cover object-center p-4 transition-transform hover:scale-105"
-    />
-  </div>
+export const ProductCoverImage = ({ src, alt }: ProductCoverImageProps): JSX.Element => (
+	<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-50">
+		<img
+			width={320}
+			height={320}
+			alt={alt}
+			src={src}
+			className="h-full w-full object-cover object-center p-4 transition-transform hover:scale-105"
+		/>
+	</div>
 );
--- src/ui/atoms/ProductDescription.tsx
+++ src/ui/atoms/ProductDescription.tsx
@@ -2,22 +2,22 @@ import { type Product } from "@/types/product";
 import { formatMoney } from "@/utils/intl";
 
 type ProductDescriptionProps = {
-  product: Product;
+	product: Product;
 };
 
 export const ProductDescription = ({
-  product: { name, category, price },
+	product: { name, category, price },
 }: ProductDescriptionProps): JSX.Element => (
-  <div className="mt-2 flex justify-between">
-    <div>
-      <h3 className="text-sm font-semibold text-gray-700">{name}</h3>
-      <p className="text-sm text-gray-500">
-        <span className="sr-only">Kategoria:</span>
-        {category}
-      </p>
-    </div>
-    <p className="text-sm font-medium text-gray-700">
-      <span className="sr-only">Cena:</span> {formatMoney(price / 100)}$
-    </p>
-  </div>
+	<div className="mt-2 flex justify-between">
+		<div>
+			<p className="text-sm font-semibold text-gray-700">{name}</p>
+			<p className="text-sm text-gray-500">
+				<span className="sr-only">Kategoria:</span>
+				{category}
+			</p>
+		</div>
+		<p className="text-sm font-medium text-gray-700">
+			<span className="sr-only">Cena:</span> {formatMoney(price / 100)}$
+		</p>
+	</div>
 );
