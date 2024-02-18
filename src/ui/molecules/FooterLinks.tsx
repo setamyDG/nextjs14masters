18ea46692638593ededfdd6aa2c4f5a30b13e5cb
Daniel Gola <danielgola@daniels-air.home>
2024-02-18 20:44:12 +0100

    feat(GENERAL): include icons in footer

--- src/ui/molecules/FooterLinks.tsx
+++ src/ui/molecules/FooterLinks.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ShoppingCart } from "lucide-react";
 import { routes } from "@/const/routes";
 
 export const FooterLinks = () => {
@@ -6,14 +7,18 @@ export const FooterLinks = () => {
 		<div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
 			<div className="md:grid md:grid-cols-2 md:gap-8">
 				<div>
-					<h3 className="text-sm font-semibold leading-6 text-gray-900">Shop</h3>
+					<div className="flex gap-4">
+						<ShoppingCart size={24} color="black" />
+						<h3 className="text-sm font-semibold leading-6 text-gray-900">Shop</h3>
+					</div>
 					<ul role="list" className="mt-6 space-y-4">
 						{routes.map((route) => (
 							<li key={route.href}>
 								<Link
-									className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+									className="flex items-center gap-4 text-sm leading-6 text-gray-600 hover:text-gray-900"
 									href={route.href}
 								>
+									{route.icon}
 									{route.label}
 								</Link>
 							</li>
